8537952a0d322cbca5b1b6eed77c97b690443e70
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2023-02-21 16:11:51 +0700

    feat : registery

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,27 +1,49 @@
 import { useLocalStorage } from "@vueuse/core";
 import { defineStore } from "pinia";
-// import { ref } from "vue";
 
+import { ref } from "vue";
 export const useUserStore = defineStore('user', () =>{
     const theme = useLocalStorage('theme', 'light')
-    const user = useLocalStorage('user', {})
+    const allUser = useLocalStorage('allUser', [])
+    const user = ref({
+        fname: '',
+        lname: '',
+        userName: '',
+        email: '',
+        password: ''
+    })
     function toggleTheme(){
         theme.value = theme.value === 'light' ? 'dark' : 'light'
     }
-    async function login(id){
-        const res = await fetch(`https://www.melivecode.com/api/users/${id}`)
-        const result = await res.json()
-        console.log(result.user);
-        user.value = result.user
+    function login(id){
+        console.log();
     }
+    function keepLocal(info){
+        console.log(info)
+        allUser.value.push(info)
+        
+    }
+    function checkUser(data){
+       allUser.value.map((item) => {
+           if(item.userName === data.userName && item.password === data.password){
+                user.value = item;
+                console.log("u are loging in")
+           }
+        })
+        console.log(user.value)
+    }
+    // async function login(id){
+    //     const res = await fetch(`https://www.melivecode.com/api/users/${id}`)
+    //     const result = await res.json()
+    //     console.log(result.user);
+    //     user.value = result.user
+    // }
 
     function logout(){
         user.value = null
-        // console.log(localStorage.getItem('user'))
-        // localStorage.removeItem('user');
     }
 
 
 
-    return { toggleTheme, theme , user, login, logout}
+    return { toggleTheme, theme , user, login, logout, keepLocal, checkUser}
 })
